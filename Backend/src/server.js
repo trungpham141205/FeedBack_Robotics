ddf84edbee1f6a2a9f66ad3bd2583da3dabd7f44
Trung Pham <pquoctrung141205@gmail.com>
2024-06-23 20:27:45 +0700

    update

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,6 +3,7 @@ require('./config/dbConfig');
 const express = require('express');
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
+const mysql = require('mysql2');
 
 const app = express();
 
@@ -12,6 +13,24 @@ configViewEngine(app);
 // Define route
 app.use('/', webRoutes);
 
+// test connection
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307,
+    user: 'root',
+    password: '141205',
+    database: 'trungpham'
+})
+
+// simple query
+connection.query(
+    'SELECT * FROM Feedback f',
+    function (error, results, fields){
+        console.log(">>>results= ", results);
+        console.log(">>>fields= ", fields);
+    }
+)
 
 app.use(express.json());
 
